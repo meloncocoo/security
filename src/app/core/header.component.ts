dd1c708a80c7fa37695611e89162e1875cd65e6c
Melon <meloncocoo@gmail.com>
2017-01-20 17:55:43 +0800

    +added ng2-cookie for language

--- src/app/core/header.component.ts
+++ src/app/core/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { TranslateService } from 'ng2-translate';
+import { Cookie } from 'ng2-cookies/ng2-cookies';
 
 @Component({
   // moduleId: module.id,
@@ -10,19 +11,18 @@ export class HeaderComponent /*implements OnInit*/ {
   @Input() public showSearchBox: boolean = false;
   @Input() public hideMenu: boolean = false;
 
-  lang: string = 'en';
-
   public isCollapsed: boolean = false;
 
   constructor(private translate: TranslateService) {
+    let lang = Cookie.get('language') || 'zh';
     this.translate.setDefaultLang('en');
-    this.translate.use(this.lang);
+    this.translate.use(lang);
   }
 
   // ngOnInit() { }
 
   setLanguage(lang: string) {
-    this.lang = lang;
-    this.translate.use(this.lang);
+    Cookie.set('language', lang);
+    this.translate.use(lang);
   }
 }
