82db9effd6974e8ee69e8b5568edfd7eb5725afc
Melon <meloncocoo@gmail.com>
2017-01-23 16:23:55 +0800

    +updated domain page

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -27,7 +27,7 @@ const ngcWebpack = require('ngc-webpack');
 const HMR = helpers.hasProcessFlag('hot');
 const AOT = helpers.hasNpmFlag('aot');
 const METADATA = {
-  title: 'Security by @Melon',
+  title: 'Security by @Well',
   baseUrl: '/',
   isDevServer: helpers.isWebpackDevServer()
 };
